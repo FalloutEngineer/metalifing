99fe211cd9704ff4da3950ffd256b72abccd1d63
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-11-30 19:19:07 +0200

    minor task page change

--- frontend/MetaLifing/app/(task)/index.tsx
+++ frontend/MetaLifing/app/(task)/index.tsx
@@ -1,15 +1,18 @@
 import Header from "@/components/Header"
+import { LayoutStyles } from "@/styles/layout"
 import React from "react"
 import { View, Text, StyleSheet } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function index() {
   return (
-    <SafeAreaView>
-      <Header isReturnButtonActive={true}>
-        <Text>Create task</Text>
-      </Header>
-      <Text>index1</Text>
-    </SafeAreaView>
+    <View style={LayoutStyles.mainView}>
+      <SafeAreaView>
+        <Header isReturnButtonActive={true}>
+          <Text>Create task</Text>
+        </Header>
+        <Text>index1</Text>
+      </SafeAreaView>
+    </View>
   )
 }
