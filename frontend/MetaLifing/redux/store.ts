24186c0e790d0e9c2c3850c0f9123ad7cc31da00
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-12-26 17:18:27 +0200

    todos redux persistence

--- frontend/MetaLifing/redux/store.ts
+++ frontend/MetaLifing/redux/store.ts
@@ -1,13 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import taskReducer from "./slices/todos"
 import createTodoReducer from "./slices/createTodo"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { persistReducer } from "redux-persist"
+import persistStore from "redux-persist/es/persistStore"
+
+const persistConfig = {
+  key: "todos",
+  storage: AsyncStorage,
+}
+
+const rootReducer = combineReducers({
+  todos: persistReducer(persistConfig, taskReducer),
+  createTodo: createTodoReducer,
+})
 
 const store = configureStore({
-  reducer: {
-    todos: taskReducer,
-    createTodo: createTodoReducer,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
